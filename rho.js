5f5fd6fa3631e2f9fd55fd6189a79ae90f957c65
Finn Rudolph <finn.rudolph@t-online.de>
2024-02-23 22:14:10 +0100

    Allow to change p and k

--- rho.js
+++ rho.js
@@ -3,6 +3,7 @@ import { getFunctionalGraph } from "./graph.js";
 
 function createSimulation(p, k) {
   const svg = d3.select("svg");
+  svg.selectAll("g").remove();
   const [nodes, links] = getFunctionalGraph(pInput.value, kInput.value);
 
   const simulation = d3
@@ -89,19 +90,7 @@ function createSimulation(p, k) {
   });
 }
 
-let pInput = document.getElementById("pInput");
-let kInput = document.getElementById("kInput");
-pInput.value = 97;
-kInput.value = 2;
-
-// Make the input fields resize according to the user input.
-for (let input of [pInput, kInput]) {
-  input.addEventListener("input", () => {
-    input.style.width = input.value.length + "ch";
-  });
-  input.dispatchEvent(new Event("input"));
-}
-
+// Set up the SVG and add it to the DOM.
 const svg = d3
   .create("svg")
   .attr("height", "100%")
@@ -125,4 +114,29 @@ svg
 
 document.querySelector("main").append(svg.node());
 
-createSimulation(pInput.value, kInput.value);
+let pInput = document.getElementById("pInput");
+let kInput = document.getElementById("kInput");
+let p = 31;
+let k = 1;
+pInput.value = p;
+kInput.value = k;
+
+// Make the input fields resize according to the user input.
+for (let input of [pInput, kInput]) {
+  input.addEventListener("input", () => {
+    input.style.width = input.value.length + "ch";
+  });
+  input.dispatchEvent(new Event("input"));
+}
+
+// If the user changes either p or k and presses `Enter`, the new graph is
+// generated.
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && (pInput.value != p || kInput.value != k)) {
+    p = pInput.value;
+    k = kInput.value;
+    createSimulation(p, k);
+  }
+});
+
+createSimulation(p, k);
