9f5be3eda8f2be507ac04d9576bb42af2bc0381c
Finn Rudolph <finn.rudolph@t-online.de>
2024-02-24 08:43:56 +0100

    Allow running the simulation automatically

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ import { createSimulation, runOneStep } from "./simulation.js";
 
 const pInput = document.getElementById("p-input");
 const kInput = document.getElementById("k-input");
+const xDisplay = document.getElementById("x-display");
+const yDisplay = document.getElementById("y-display");
+const playButton = document.getElementById("play");
+const stepButton = document.getElementById("step");
+
 let p = 69;
 let k = 1;
+let x = 1;
+let y = 1;
+
 pInput.value = p;
 kInput.value = k;
+xDisplay.textContent = x;
+yDisplay.textContent = y;
+playButton.textContent = ">";
+stepButton.textContent = ">>";
+
+let playing = false;
+let intervalId;
 
 // Make the input fields resize according to the user input.
 for (let input of [pInput, kInput]) {
@@ -21,35 +36,36 @@ window.addEventListener("keydown", (event) => {
   if (event.key === "Enter" && (pInput.value != p || kInput.value != k)) {
     p = pInput.value;
     k = kInput.value;
+    x = 1;
+    y = 1;
+    xDisplay.textContent = x;
+    yDisplay.textContent = y;
+    if (playing) {
+      playButton.dispatchEvent(new Event("click"));
+    }
     createSimulation(p, k, x, y);
   }
 });
 
-const xDisplay = document.getElementById("x-display");
-const yDisplay = document.getElementById("y-display");
-const playButton = document.getElementById("play");
-const stepButton = document.getElementById("step");
-let x = 1;
-let y = 1;
-xDisplay.textContent = x;
-yDisplay.textContent = y;
-playButton.textContent = ">";
-stepButton.textContent = ">>";
-
 createSimulation(p, k, x, y);
-
-let playing = false;
 
 playButton.addEventListener("click", () => {
   if (playing) {
-    playButton.textContent = "||";
-  } else {
     playButton.textContent = ">";
+    clearInterval(intervalId);
+  } else {
+    playButton.textContent = "||";
+    intervalId = setInterval(() => {
+      [x, y] = runOneStep(p, k, x, y);
+      xDisplay.textContent = x;
+      yDisplay.textContent = y;
+    }, 600);
   }
   playing ^= 1;
 });
 
 stepButton.addEventListener("click", () => {
   [x, y] = runOneStep(p, k, x, y);
-  console.log(x, y);
+  xDisplay.textContent = x;
+  yDisplay.textContent = y;
 });
